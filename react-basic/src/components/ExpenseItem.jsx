c66e86f7c0d465df4a23f476c233b5f874f19ee9
yatharth <lakhatey@gmail.com>
2023-01-04 21:49:28 +0530

    Created expense date component

--- react-basic/src/components/ExpenseItem.jsx
+++ react-basic/src/components/ExpenseItem.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem({ title, amount, LocationOfExpenditure, date }) {
-  const month = date.toLocaleString("en-US", { month: "long" });
-  const year = date.getFullYear();
-  const day = date.toLocaleString("en-US", { day: "2-digit" });
   return (
     <div className="expense-item">
-      <div>
-        {month} {day} {year}
-      </div>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <h3>{LocationOfExpenditure}</h3>
