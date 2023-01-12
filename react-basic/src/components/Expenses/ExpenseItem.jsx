f38e86fda07cb7bdb742669fd6c86982dc1f955f
yatharth <lakhatey@gmail.com>
2023-01-12 16:10:32 +0530

    clean up

--- react-basic/src/components/Expenses/ExpenseItem.jsx
+++ react-basic/src/components/Expenses/ExpenseItem.jsx
@@ -5,38 +5,38 @@ import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 
 const ExpenseItem = ({ title, amount, LocationOfExpenditure, date }) => {
-  const [titleState, setTitle] = useState(title);
-  const [amountState, seAmount] = useState(amount);
+  // const [titleState, setTitle] = useState(title);
+  // const [amountState, seAmount] = useState(amount);
 
   // console.log("EXPENSE item is rendered");
 
-  const clickHandler = () => {
-    console.log("clicked");
+  // const clickHandler = () => {
+  //   console.log("clicked");
 
-    setTitle("new Title");
-  };
+  //   setTitle("new Title");
+  // };
 
-  const add100toAmount = () => {
-    seAmount(amountState + 100);
-  };
+  // const add100toAmount = () => {
+  //   seAmount(amountState + 100);
+  // };
 
-  const deleteExpense = (event) => {
-    let ele = event.target.parentElement;
-    // console.log(ele);
-    ele.remove();
-  };
+  // const deleteExpense = (event) => {
+  //   let ele = event.target.parentElement;
+  //   // console.log(ele);
+  //   ele.remove();
+  // };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <ExpenseDetails
-        title={titleState}
+        title={title}
         LocationOfExpenditure={LocationOfExpenditure}
-        amount={amountState}
+        amount={amount}
       />
-      <button onClick={clickHandler}>Change Title </button>
+      {/* <button onClick={clickHandler}>Change Title </button>
       <button onClick={deleteExpense}>Delete</button>
-      <button onClick={add100toAmount}>Add 100$</button>
+      <button onClick={add100toAmount}>Add 100$</button> */}
     </Card>
 
     // THIS happen with JSX under the hood
